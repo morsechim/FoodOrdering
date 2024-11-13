3c4e5c9e885ff7dc95e85fb247192ff6bca15217
morsechim <mr.morsechim@gmail.com>
2024-11-13 21:11:48 +0700

    create product screen

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import { router, Stack, useLocalSearchParams } from "expo-router";
+
+import Colors from "@/constants/Colors";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+
+import products from "@assets/data/products";
+
+import Button from "@/components/Button";
+import { useCart } from "@/providers/CardProvider";
+import { PizzaSize } from "@/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+
+  const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaImage }}
+      />
+
+      <Text style={styles.price}>${product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+      {/* <Button onPress={addToCart} text="Add to cart" /> */}
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
---
+++ src/app/(admin)/menu/create.tsx
@@ -0,0 +1,88 @@
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import Button from "@/components/Button";
+
+const CreateProductScreen = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const [errors, setErrors] = useState("");
+
+  const onCreate = () => {
+    // if not validated
+    if (!validateInput()) return;
+
+    console.warn("Creating product:", name);
+
+    // Save in the database
+
+    resetFields();
+  };
+
+  const validateInput = () => {
+    setErrors("");
+
+    if (!name) {
+      setErrors("Name is required");
+      return false;
+    }
+    if (!price) {
+      setErrors("Price is required");
+      return false;
+    }
+    if (isNaN(parseFloat(price))) {
+      setErrors("Price is not a number");
+      return false;
+    }
+  };
+
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+
+      <Text style={{ color: "red" }}>{errors}</Text>
+      <Button onPress={onCreate} text="Create" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+  label: {
+    color: "gray",
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+});
+
+export default CreateProductScreen;
